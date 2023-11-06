4621c1656c54de3096bd1af63d765eb68e006916
DucChanHUST <kuntakufcvn@gmail.com>
2023-11-06 10:21:26 +0700

    color

--- front_end/src/componets/Map/MapComponent.jsx
+++ front_end/src/componets/Map/MapComponent.jsx
@@ -1,18 +1,30 @@
-import React from "react";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import React, { Fragment } from "react";
+import { MapContainer, Marker, TileLayer, Popup, Polyline } from "react-leaflet";
+import L from 'leaflet';
 import { useMapEvent } from "react-leaflet/hooks";
-import { useStore } from "../../store";
+import { useStore } from "../../store"; 
+
+const markerIcons = [];
+for (let i = 1; i <= 5; i++) {
+  const icon = new L.Icon({
+    iconUrl: require(`../../resource/location-pin${i}.png`),
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  markerIcons.push(icon);
+}
 
 const MapComponent = () => {
   const [state, dispatch] = useStore();
+  const line = state.map(address => address.position)
   const addPosition = (latlng) => {
     dispatch({
-      type: "ADD_POS",
+      type: "ADD_POS_BY_CLICK",
       name: `[${latlng.lat}, ${latlng.lng}]`,
       position: [latlng.lat, latlng.lng],
     });
   };
-
+  console.log(state);
   const MapClick = () => {
     const map = useMapEvent("click", (e) => {
       addPosition(e.latlng);
@@ -21,12 +33,13 @@ const MapComponent = () => {
     return (
       <>
         {state.map((address, index) => (
-          <Marker key={index} position={address.position}>
+          <Marker key={index} position={address.position} icon={markerIcons[index%5]}>
             <Popup>
               position {index+1}
             </Popup>
           </Marker>
-        ))}
+          ))}
+          {(state) ? <Polyline positions={line} color="#F45990" weight={3} opacity={0.9} /> : <Fragment />}
       </>
     );
   };
--- front_end/src/store/reducer.js
+++ front_end/src/store/reducer.js
@@ -2,11 +2,16 @@ const initState = [];
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_POS':
+    case 'ADD_POS_BY_CLICK':
       return [...state, {
         name: action.name,
         position: action.position
       }];
+    case 'ADD_POS_BY_SEARCH':
+        return [...state, {
+          name: action.name,
+          position: [action.position[1], action.position[0]]
+        }];
     case 'DELETE_POS':
       return state.filter(pos => pos.position !== action.position)
     default:
