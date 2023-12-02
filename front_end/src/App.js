5129f602775e6045452c089f2cde05212626c69b
DucChanHUST <kuntakufcvn@gmail.com>
2023-12-02 09:12:42 +0700

    filter addresses

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,7 +3,6 @@ import { Grid } from "@material-ui/core";
 
 import Features from "./componets/Features/Features";
 import MapComponent from "./componets/Map/MapComponent";
-import PlaceDetails from "./componets/PlaceDetails/PlaceDetails";
 
 const App = () => {
 
@@ -12,7 +11,6 @@ const App = () => {
       <Grid container spacing={1}>
         <Grid item xs={3}>
           <Features />
-          <PlaceDetails />
         </Grid>
         <Grid item xs={9}>
           <MapComponent />
